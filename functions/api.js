90740668f43165ac7c49bce2e5fd054fd9bf6db5
jerome <jeromelingon02@gmail.com>
2024-06-10 08:06:13 +0800

    add

---
+++ functions/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const serverless= require('serverless-http');
+const router = require('./routes/author');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const dbCloudUrl = 
+const dbLocalUrl = 
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+mongoose
+	.connect(dbCloudUrl || dbLocalUrl)
+	.then(() => console.log('Connected to MongoDB'))
+	.catch((error) => console.error('Failed to connct to MongoDB', error));
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app)
