f0c353ff600c616b8db72658d4d632560f90640e
Waves21 <jeromelingon02@gmail.com>
2024-06-10 20:07:45 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 const dbCloudUrl = 'mongodb+srv://rayo:Sekome123@cluster0.mns7uso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const dbLocalUrl = 'mongodb://localhost:27017/rayo-serverless';
+const dbLocalUrl = 'mongodb://localhost:27017/rayo-serverless.name';
 
 app.use(cors());
 app.use(express.json());
