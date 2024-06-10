c2b7eb7b0dc9da7ede881e7a33bfe8a860dd6216
Waves21 <jeromelingon02@gmail.com>
2024-06-10 08:57:07 +0800

    Update author.js

--- functions/models/author.js
+++ functions/models/author.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const authorSchema = require('../schema/author');
 
-const AuthorModel = mongoos.model('Author', authorSchema);
+const AuthorModel = mongoose.model('Author', authorSchema);
 
 module.exports = AuthorModel;
